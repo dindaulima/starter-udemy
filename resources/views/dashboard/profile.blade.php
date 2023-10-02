da786c374d35708aa957166998576ec06a74d9b2
Diul <dindaulima@gmail.com>
2023-10-02 16:20:35 +0700

    add stylind profile using css and js bootstrap cdn

---
+++ resources/views/dashboard/profile.blade.php
@@ -0,0 +1,169 @@
+@extends('layouts.app')
+
+@section('title','Edit Profile')
+
+@section('content')
+<section class="section">
+    <div class="section-header">
+    <h1>Edit Profile</h1>
+    </div>
+
+    <div class="section-body">
+    <h2 class="section-title">Hi, Ujang!</h2>
+            <p class="section-lead">
+              Change information about yourself on this page.
+            </p>
+
+            <div class="row mt-sm-4">
+              <div class="col-12 col-md-12 col-lg-5">
+                <div class="card profile-widget">
+                  <div class="profile-widget-header">
+                    <img alt="image" src="../assets/img/avatar/avatar-1.png" class="rounded-circle profile-widget-picture">
+                    <div class="profile-widget-items">
+                      <div class="profile-widget-item">
+                        <div class="profile-widget-item-label">Posts</div>
+                        <div class="profile-widget-item-value">187</div>
+                      </div>
+                      <div class="profile-widget-item">
+                        <div class="profile-widget-item-label">Followers</div>
+                        <div class="profile-widget-item-value">6,8K</div>
+                      </div>
+                      <div class="profile-widget-item">
+                        <div class="profile-widget-item-label">Following</div>
+                        <div class="profile-widget-item-value">2,1K</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="profile-widget-description">
+                    <div class="profile-widget-name">Ujang Maman <div class="text-muted d-inline font-weight-normal"><div class="slash"></div> Web Developer</div></div>
+                    Ujang maman is a superhero name in <b>Indonesia</b>, especially in my family. He is not a fictional character but an original hero in my family, a hero for his children and for his wife. So, I use the name as a user in this template. Not a tribute, I'm just bored with <b>'John Doe'</b>.
+                  </div>
+                  <div class="card-footer text-center">
+                    <div class="font-weight-bold mb-2">Follow Ujang On</div>
+                    <a href="#" class="btn btn-social-icon btn-facebook mr-1">
+                      <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="#" class="btn btn-social-icon btn-twitter mr-1">
+                      <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="#" class="btn btn-social-icon btn-github mr-1">
+                      <i class="fab fa-github"></i>
+                    </a>
+                    <a href="#" class="btn btn-social-icon btn-instagram">
+                      <i class="fab fa-instagram"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 col-md-12 col-lg-7">
+                <div class="card">
+                  <form method="post" class="needs-validation" novalidate="">
+                    <div class="card-header">
+                      <h4>Edit Profile</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                          <div class="form-group col-md-6 col-12">
+                            <label>First Name</label>
+                            <input type="text" class="form-control" value="Ujang" required="">
+                            <div class="invalid-feedback">
+                              Please fill in the first name
+                            </div>
+                          </div>
+                          <div class="form-group col-md-6 col-12">
+                            <label>Last Name</label>
+                            <input type="text" class="form-control" value="Maman" required="">
+                            <div class="invalid-feedback">
+                              Please fill in the last name
+                            </div>
+                          </div>
+                        </div>
+                        <div class="row">
+                          <div class="form-group col-md-7 col-12">
+                            <label>Email</label>
+                            <input type="email" class="form-control" value="ujang@maman.com" required="">
+                            <div class="invalid-feedback">
+                              Please fill in the email
+                            </div>
+                          </div>
+                          <div class="form-group col-md-5 col-12">
+                            <label>Phone</label>
+                            <input type="tel" class="form-control" value="">
+                          </div>
+                        </div>
+                        <div class="row">
+                          <div class="form-group col-12">
+                            <label>Bio</label>
+                            <textarea class="form-control summernote-simple">Ujang maman is a superhero name in <b>Indonesia</b>, especially in my family. He is not a fictional character but an original hero in my family, a hero for his children and for his wife. So, I use the name as a user in this template. Not a tribute, I'm just bored with <b>'John Doe'</b>.</textarea>
+                          </div>
+                        </div>
+                        <div class="row">
+                          <div class="form-group mb-0 col-12">
+                            <div class="custom-control custom-checkbox">
+                              <input type="checkbox" name="remember" class="custom-control-input" id="newsletter">
+                              <label class="custom-control-label" for="newsletter">Subscribe to newsletter</label>
+                              <div class="text-muted form-text">
+                                You will get new information about products, offers and promotions
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                    <div class="card-footer text-right">
+                      <button class="btn btn-primary">Save Changes</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+    </div>
+</section>
+@endsection
+
+@section('sidebar')
+    @parent
+    <li class="menu-header">Starter</li>
+    <li class="nav-item dropdown">
+    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
+    <ul class="dropdown-menu">
+        <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
+        <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
+        <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
+    </ul>
+    </li>
+    <li class="active"><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+    <li class="nav-item dropdown">
+    <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Bootstrap</span></a>
+    <ul class="dropdown-menu">
+        <li><a class="nav-link" href="bootstrap-alert.html">Alert</a></li>
+        <li><a class="nav-link" href="bootstrap-badge.html">Badge</a></li>
+        <li><a class="nav-link" href="bootstrap-breadcrumb.html">Breadcrumb</a></li>
+        <li><a class="nav-link" href="bootstrap-buttons.html">Buttons</a></li>
+        <li><a class="nav-link" href="bootstrap-card.html">Card</a></li>
+        <li><a class="nav-link" href="bootstrap-carousel.html">Carousel</a></li>
+        <li><a class="nav-link" href="bootstrap-collapse.html">Collapse</a></li>
+        <li><a class="nav-link" href="bootstrap-dropdown.html">Dropdown</a></li>
+        <li><a class="nav-link" href="bootstrap-form.html">Form</a></li>
+        <li><a class="nav-link" href="bootstrap-list-group.html">List Group</a></li>
+        <li><a class="nav-link" href="bootstrap-media-object.html">Media Object</a></li>
+        <li><a class="nav-link" href="bootstrap-modal.html">Modal</a></li>
+        <li><a class="nav-link" href="bootstrap-nav.html">Nav</a></li>
+        <li><a class="nav-link" href="bootstrap-navbar.html">Navbar</a></li>
+        <li><a class="nav-link" href="bootstrap-pagination.html">Pagination</a></li>
+        <li><a class="nav-link" href="bootstrap-popover.html">Popover</a></li>
+        <li><a class="nav-link" href="bootstrap-progress.html">Progress</a></li>
+        <li><a class="nav-link" href="bootstrap-table.html">Table</a></li>
+        <li><a class="nav-link" href="bootstrap-tooltip.html">Tooltip</a></li>
+        <li><a class="nav-link" href="bootstrap-typography.html">Typography</a></li>
+    </ul>
+    </li>
+@endsection
+
+@push('customCss')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.20/summernote-bs4.min.css">
+@endpush
+
+@push('customJs')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.20/summernote-bs4.min.js"></script>
+@endpush
